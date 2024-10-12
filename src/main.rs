2f25ad7f958d659a79d55fbc874b11f41db6c3c3
jameshc2001 <jameshcrofts@gmail.com>
2024-10-12 19:11:29 +0100

    Added bevy

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,13 @@
+use bevy::app::App;
+use bevy::color::Color;
+use bevy::DefaultPlugins;
+use bevy::prelude::ClearColor;
+
+const BACKGROUND_COLOR: Color = Color::BLACK;
+
 fn main() {
-    println!("Hello, world!");
+    App::new()
+        .add_plugins(DefaultPlugins)
+        .insert_resource(ClearColor(BACKGROUND_COLOR))
+        .run();
 }
